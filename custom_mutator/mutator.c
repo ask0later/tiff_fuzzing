ec6720d4d0e5aee34b2116c4bd1d2ae1b87a0428
Yaroslav Kuznetsov <asklater@bk.ru>
2024-07-17 21:36:11 +0300

    update mutator

--- custom_mutator/mutator.c
+++ custom_mutator/mutator.c
@@ -1,21 +1,30 @@
 
-#include "../../../AFL__/AFLplusplus/include/afl-fuzz.h"
-#include <X11/X.h>
+#include "afl-fuzz.h"
 #include <stdlib.h>
 
-#define DATA_SIZE 8388607
+#define DATA_SIZE 0x7FFFFF
 #define RANDOM_IF(code_1, code_2) if (rand() % 2) {code_1;} else {code_2;}
+
+enum DE_data_type
+{
+        BYTE     = 1,
+        ASCII    = 2,
+        INT      = 3,
+        LONG     = 4,
+        RATIONAL = 5
+};
+
+enum Attribute_tagID
+{
+        IMAGE_WIDTH = 0x0100, 	 
+	IMAGE_HIGH  = 0x0101
+};
+
 
 typedef struct my_custom_mutator
 {
         afl_state_t *afl;
-
-        // any additional data here!
-        size_t trim_size_current;
-        int    trimmming_steps;
-        int    cur_step;
-
-  u8 *mutated_out, *post_process_buf, *trim_buf;
+        uint8_t *mutated_out;
 } my_custom_mutator_t;
 
 typedef struct 
@@ -39,8 +48,7 @@ typedef struct
 typedef struct 
 {
         unsigned short DE_count;
-        TIFF_DE DEs[1];
-
+        // there are the array of TIFF_DE
         unsigned int next_IFD_offset;
 
 } TIFF_IFD; // Image File Directory 
@@ -69,33 +77,17 @@ void *afl_custom_init(afl_state_t *afl, unsigned int seed)
                 return NULL;
         }
         
-        if ((data->mutated_out = (u8 *)malloc(MAX_FILE)) == NULL) 
+        if ((data->mutated_out = (uint8_t*)malloc(MAX_FILE)) == NULL) 
         {
                 perror("afl_custom_init: allocation error");
                 return NULL;
 
         }
-
-        if ((data->post_process_buf = (u8 *)malloc(MAX_FILE)) == NULL) 
-        {
-                perror("afl_custom_init: allocation error");
-                return NULL;
-
-        }
-
-        if ((data->trim_buf = (u8 *)malloc(MAX_FILE)) == NULL) 
-        {
-                perror("afl_custom_init: allocation error");
-                return NULL;
-        }
-
 
         data->afl = afl;
 
         return data;
 }
-
-
 
 
 /**
@@ -106,12 +98,9 @@ void *afl_custom_init(afl_state_t *afl, unsigned int seed)
 
 void afl_custom_deinit(my_custom_mutator_t *data) 
 {
-        free(data->post_process_buf);
         free(data->mutated_out);
-        free(data->trim_buf);
         free(data);
 }
-
 
 /**
 * Perform custom mutations on a given input
@@ -133,7 +122,7 @@ void afl_custom_deinit(my_custom_mutator_t *data)
 */
 
 size_t afl_custom_fuzz(my_custom_mutator_t *data, uint8_t *buf, size_t buf_size,
-                       u8 **out_buf, uint8_t *add_buf,
+                       uint8_t **out_buf, uint8_t *add_buf,
                        size_t add_buf_size,  // add_buf can be NULL
                        size_t max_size)
 {
@@ -159,38 +148,39 @@ size_t mutate_tiff_file(unsigned char * buffer, size_t mutated_size)
 
         size_t offset = hdr->tiff_diroff <= mutated_size ? hdr->tiff_diroff : mutated_size;
 
-        RANDOM_IF(buffer[0] = 0x49, buffer[0] = 0x4d)
-        RANDOM_IF(buffer[1] = 0x49, buffer[1] = 0x4d)
+        if (offset < sizeof(TIFF_IFH))
+                return offset;
 
-        RANDOM_IF(buffer[2] = 0x00, buffer[2] = 0x2a)
+        RANDOM_IF(buffer[0] = 0x49, buffer[0] = 0x4d)   // 4949 - little-endian
+        RANDOM_IF(buffer[1] = 0x49, buffer[1] = 0x4d)   // 4d4d - big-endian
+
+        RANDOM_IF(buffer[2] = 0x00, buffer[2] = 0x2a)   // 0x2a - TIFF flag
         RANDOM_IF(buffer[3] = 0x00, buffer[3] = 0x2a)
 
         RANDOM_IF(, unsigned int tmp = rand(); memcpy(buffer + 4, (unsigned char*) &tmp, 4))
 
         // from sizeof(TIFF_IFH) to offset -- pixel image data
         RANDOM_IF(, for (size_t i = sizeof(TIFF_IFH); i < offset; i++){buffer[i] &= (rand() % 255);})
-        
 
         TIFF_IFD* first_IFD = (TIFF_IFD*) (buffer + offset);
         size_t DE_count = first_IFD->DE_count;
 
-
-
-        if (offset + DE_count * sizeof(TIFF_DE) + 6 <= mutated_size)
+        if (offset + DE_count * sizeof(TIFF_DE) + sizeof(TIFF_IFD) <= mutated_size)
         {
                 RANDOM_IF(first_IFD->DE_count = (unsigned short) rand(), )
 
-                TIFF_DE* current = (TIFF_DE*) (buffer + offset + 2);
+                TIFF_DE* current = (TIFF_DE*) (buffer + offset + sizeof(first_IFD->DE_count));
 
                 for (size_t DE_index = 0; DE_index < DE_count; DE_index++)
                 {
-                        if (current->tag == 0x0100 || current->tag == 0x0101)
+                        if (current->tag == IMAGE_WIDTH || current->tag == IMAGE_HIGH)
                         {
-                                current->type = 0x03;
-                                current->length = 0x1;
+                                current->type   = INT;
+                                current->length = 1;
                                 current->value_offset = rand();
                                 continue;
                         }
+
                         RANDOM_IF(current->tag          = rand(),)
                         RANDOM_IF(current->type         = rand(),)
                         RANDOM_IF(current->length       = rand(),)
@@ -205,10 +195,6 @@ size_t mutate_tiff_file(unsigned char * buffer, size_t mutated_size)
                         buffer[i] &= (rand() % 255);
                 }
         }
-
-        
-
-        
 
         return offset;
 }
